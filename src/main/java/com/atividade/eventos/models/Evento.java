2342f39c2d149038183d5f8f9e0346b53a1eae64
victordev1 <victor.souza97@outlook.com>
2021-06-08 16:41:08 -0300

    Configurando classe de modelo Evento

--- src/main/java/com/atividade/eventos/models/Evento.java
+++ src/main/java/com/atividade/eventos/models/Evento.java
@@ -1,11 +1,28 @@
 package com.atividade.eventos.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Evento {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
