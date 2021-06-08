bfd14b89272deb00b11541b771e210b3b08e8696
victordev1 <victor.souza97@outlook.com>
2021-06-08 16:10:35 -0300

    Criando o controller de eventos

---
+++ src/main/java/com/atividade/eventos/controllers/EventosController.java
@@ -0,0 +1,14 @@
+package com.atividade.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventosController {
+	
+	 @RequestMapping("/eventos/form")
+	 public String form() {
+		 return "formEvento";
+	 }
+
+}
