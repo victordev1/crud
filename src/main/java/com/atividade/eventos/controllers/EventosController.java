5df83fe36d31f71b3310085c196628a0af68de95
victordev1 <victor.souza97@outlook.com>
2021-06-09 16:44:40 -0300

    Criando pagina de detalhes do evento

--- src/main/java/com/atividade/eventos/controllers/EventosController.java
+++ src/main/java/com/atividade/eventos/controllers/EventosController.java
@@ -1,10 +1,12 @@
 package com.atividade.eventos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,13 +34,30 @@ public class EventosController {
 
 		return "eventos/evento-adicionado";
 	}
-	
+
 	@GetMapping
-	public ModelAndView listar() {		
+	public ModelAndView listar() {
 		List<Evento> eventos = er.findAll();
 		ModelAndView mv = new ModelAndView("eventos/lista");
 		mv.addObject("eventos", eventos);
 		return mv;
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Evento> opt = er.findById(id);
+
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/eventos");
+			return md;
+		}
+
+		md.setViewName("eventos/detalhes");
+		Evento evento = opt.get();
+		md.addObject("evento", evento);
+
+		return md;
 	}
 
 }
